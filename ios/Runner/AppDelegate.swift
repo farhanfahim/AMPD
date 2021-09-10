e3619c74dbed2e270f581a061ab0000deed667d9
ejaz <ejaz@ejazs-MacBook-Pro.local>
2021-09-10 18:37:45 +0500

    notifications done

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,22 @@
 import UIKit
 import Flutter
 import Firebase
+import FirebaseMessaging
+
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
 
-    FirebaseApp.configure()
+    if #available(iOS 10.0, *) {
+           UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+         }
+         FirebaseApp.configure()
+         Messaging.messaging().delegate = self
+
     GeneratedPluginRegistrant.register(with: self)
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
